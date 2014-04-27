c427707ba289152dec1f31404b93a9b338251805
Ana Lacerda <anaclara.lacerdas@gmail.com>
2014-04-27 17:23:09 -0300

    flufy index page, header and footer

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'slim'
 
+helpers do
+  def include_slim(name, options = {}, &block)
+  	if name == 'header'
+  		slim :header
+  	elsif name == 'footer'
+		slim :footer
+  	end
+  end
+end
+
 get '/' do
   slim :index
 end
