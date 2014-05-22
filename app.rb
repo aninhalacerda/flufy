b8aafd3f4b5a0b852f1eb3dbb6ac5623a51ce095
Ana Lacerda <anaclara.lacerdas@gmail.com>
2014-05-22 17:29:37 -0300

    database settings for all environments

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'yaml'
 require 'pry'
 require 'sinatra-env'
 
-DB_CONFIG = YAML::load(File.open('config/database.yml'))[Sinatra.env]
+DB_CONFIG = YAML::load(File.open('config/database.yml'))
 set :database, "mysql://#{DB_CONFIG['username']}:#{DB_CONFIG['password']}@#{DB_CONFIG['host']}:#{DB_CONFIG['port']}/#{DB_CONFIG['database']}"
 
 helpers do
