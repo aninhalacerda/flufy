818e6689efdb594accf24a5ac0559fab03ef4a87
Ana Lacerda <anaclara.lacerdas@gmail.com>
2014-05-22 19:47:14 -0300

    configuring DATABASE_URL connection for production env

--- app.rb
+++ app.rb
@@ -5,8 +5,16 @@ require 'yaml'
 require 'pry'
 require 'sinatra-env'
 
-DB_CONFIG = YAML.load(ERB.new(File.read(File.join("config","database.yml"))).result)
-set :database, "postgresql://#{DB_CONFIG['username']}:#{DB_CONFIG['password']}@#{DB_CONFIG['host']}:#{DB_CONFIG['port']}/#{DB_CONFIG['database']}"
+DB_CONFIG = YAML.load(ERB.new(File.read(File.join("config","database.yml"))).result)[Sinatra.env]
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'])
+  set :database, "postgresql://#{DB_CONFIG['username']}:#{DB_CONFIG['password']}@#{db.host}:#{db.port}/#{db.path[1..-1]}"
+end
+
+configure :development do
+  set :database, "postgresql://#{DB_CONFIG['username']}:#{DB_CONFIG['password']}@#{DB_CONFIG['host']}:#{DB_CONFIG['port']}/#{DB_CONFIG['database']}"
+end
 
 helpers do
   def include_slim(name, options = {}, &block)
